347ffab8bffb24c64ba450aac82c125ff7749c83
kaolsi <ruth96ortega@gmail.com>
2018-10-15 20:03:48 +0200

    L1

--- sumaFiles.js
+++ sumaFiles.js
@@ -1,4 +1,30 @@
+// SÍNCRONO:
+/*
 const fs = require('fs')
 const numero1 = fs.readFileSync('./numero1', 'utf-8')
 const numero2 = fs.readFileSync('./numero2', 'utf-8')
 console.log(`El resultado de la suma es  ${parseInt(numero1)+parseInt(numero2)}`)
+*/
+
+// ASÍNCRONO (CALLBACK FUNCTIONS):
+
+const fs = require('fs')
+
+fs.readFile('./numero1', 'utf-8', (err, numero1) => {
+  if (err) throw err
+  fs.readFile('./numero2', 'utf-8', (err, numero2) => {
+    if (err) throw err
+    console.log(`El resultado de la suma es ${parseInt(numero1) + parseInt(numero2)}`)
+  })
+})
+// node sumaFiles
+
+// ASÍNCRONO CON ESTILO SÍNCRONO (PROMESAS):
+const customReadFile = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('fileName', (err, data) => {
+      if (err) reject()
+      return resolve(data)
+    })
+  })
+}
